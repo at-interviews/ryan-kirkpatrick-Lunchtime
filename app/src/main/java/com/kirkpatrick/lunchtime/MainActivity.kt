38ace4ed64e307ec5a14dc140d1da9a8ec444c8a
Ryan Kirkpatrick <ryank262@gmail.com>
2024-09-24 09:52:56 +1000

    init places api

--- app/src/main/java/com/kirkpatrick/lunchtime/MainActivity.kt
+++ app/src/main/java/com/kirkpatrick/lunchtime/MainActivity.kt
@@ -1,27 +1,37 @@
 package com.kirkpatrick.lunchtime
 
+import android.R.attr.data
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.common.api.Status
 import com.google.android.libraries.places.api.Places
-import com.kirkpatrick.lunchtime.ui.theme.LunchtimeTheme
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import com.kirkpatrick.lunchtime.databinding.MainActivityLayoutBinding
+import java.util.Locale
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity_layout)
+        binding = MainActivityLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initPlacesApi()
+
+        binding.searchEditText.apply {
+            setOnFocusChangeListener { _, hasFocus ->
+                if(hasFocus) { startAutoCompleteIntent() }
+            }
+            setOnClickListener { startAutoCompleteIntent() }
+        }
     }
 
     private fun initPlacesApi() {
@@ -36,25 +46,46 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Initialize the SDK
-        Places.initializeWithNewPlacesApiEnabled(applicationContext, apiKey)
+        Places.initialize(applicationContext, apiKey)
 
         // Create a new PlacesClient instance
         val placesClient = Places.createClient(this)
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private val startAutocomplete = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == RESULT_OK) {
+            val intent = result.data
+            val status: Status = Autocomplete.getStatusFromIntent(result.data)
+            Log.e(TAG, status.toString())
+            if (intent != null) {
+                val place = Autocomplete.getPlaceFromIntent(intent)
+                binding.searchEditText.setText(place.name)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LunchtimeTheme {
-        Greeting("Android")
+                // Write a method to read the address components from the Place
+                // and populate the form with the address components
+                Log.d(TAG, "Place: " + place.addressComponents)
+                //fillInAddress(place)
+            }
+        } else if (result.resultCode == RESULT_CANCELED) {
+            // The user canceled the operation.
+            Log.i(TAG, "User canceled autocomplete")
+        }
+    }
+
+    private fun startAutoCompleteIntent() {
+        // Set the fields to specify which types of place data to
+        // return after the user has made a selection.
+        val fields = listOf(Place.Field.ID, Place.Field.LOCATION, Place.Field.DISPLAY_NAME)
+
+        // Start the autocomplete intent.
+        val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
+            .build(this)
+        startAutocomplete.launch(intent)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
